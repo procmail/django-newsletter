3f82b2a2f02877602e4c74d062adf1081d5a1f20
Eric Boo <ericboo@gmail.com>
2014-09-27 19:55:48 +0800

    Removed import to prevent circular importing error

--- customer/models.py
+++ customer/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-# from purchase.models import Purchase
-from newsletter.models import Subscription, Submission, Blacklist
+# from newsletter.models import Subscription  # importing this causes an Internal Server Error, caused by a circular import
 
 
 class Customer(models.Model):
 	# This is not a django user, but a Subscription
 	user = models.ForeignKey(
-	    Subscription, blank=True, null=True, verbose_name=_('user')
+	    "Subscription", blank=True, null=True, verbose_name=_('user')
 	)
 	# purchases = models.ManyToManyField(Purchase)
 
